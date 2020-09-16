fe4374a5c8e22b0ecdb99bd8c7da2692581a38f7
Rajorshi Roy <35338951+royz@users.noreply.github.com>
2020-09-16 20:02:43 +0530

    moved chromedriver initialization to main

--- investing.py
+++ investing.py
@@ -9,6 +9,7 @@ import requests
 import tkinter as tk
 from tkinter import ttk
 import config
+from datetime import datetime
 
 
 class Investing:
@@ -21,11 +22,8 @@ class Investing:
         self.root = None
         self.table = None
 
-    def login(self):
-        options = Options()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        self.driver = webdriver.Chrome(options=options)
+    def login(self, driver):
+        self.driver = driver
         print('opening login page')
         self.driver.get('https://www.investing.com/')
         print('waiting for page to load')
@@ -43,7 +41,6 @@ class Investing:
         self.driver.get('https://www.investing.com/portfolio/')
 
     def update_data(self):
-        print('updating data...')
         try:
             soup = BeautifulSoup(self.driver.page_source, 'html.parser')
             table_rows = soup.find('table', {'class': 'js-table-sortable'}).find('tbody').find_all('tr')
@@ -71,34 +68,37 @@ class Investing:
             self.table.column(header, anchor=tk.W, width=100, minwidth=100)
             self.table.heading(header, text=header, anchor=tk.W)
 
+        # format tags
+        self.table.tag_configure('red', background='#ffcbcb')
+        self.table.tag_configure('green', background='#99f3bd')
+
         # add data
         for i, row in enumerate(self.data):
-            self.table.insert(parent='', index='end', iid=i, values=row)
+            self.table.insert(parent='', index='end', iid=i, values=row, tags=self.get_tags(row[2]))
+
+    @staticmethod
+    def get_tags(chg):
+        chg = float(chg[:-1])
+        if chg > 0:
+            tags = ('green',)
+        elif chg < 0:
+            tags = ('red',)
+        else:
+            tags = (None,)
+        return tags
 
     def update_window(self):
-        print('updating...')
+        print(f'[{datetime.now()}] updating data and window...')
         try:
             # update data from site
             self.update_data()
 
             # destroy previous table
-            for wd in self.root.winfo_children():
-                wd.destroy()
-
-            # add the updated table
-            self.table = ttk.Treeview(self.root, height=len(self.data))
-            self.table.pack(side=tk.TOP, fill=tk.X)
-            self.table['columns'] = self.headers
-            self.table.column('#0', width=0, stretch=tk.NO)
-
-            # format columns and add headings
-            for header in self.headers:
-                self.table.column(header, anchor=tk.W, width=100, minwidth=100)
-                self.table.heading(header, text=header, anchor=tk.W)
+            self.table.delete(*self.table.get_children())
 
             # add data
             for i, row in enumerate(self.data):
-                self.table.insert(parent='', index='end', iid=i, values=row)
+                self.table.insert(parent='', index='end', iid=i, values=row, tags=self.get_tags(row[2]))
 
             self.root.after(1000, self.update_window)
         except:
@@ -113,19 +113,23 @@ if __name__ == '__main__':
     if mode == '1':
         interval = int(input('enter an update interval in minutes: '))
 
-    investing = Investing()
-    investing.login()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    with webdriver.Chrome(options=options) as chrome_driver:
+        investing = Investing(chrome_driver)
+        investing.login()
 
-    if mode == '1':
-        while True:
+        if mode == '1':
+            while True:
+                investing.update_data()
+                investing.render_window()
+                investing.root.mainloop()
+                time.sleep(interval * 60)
+        elif mode == '2':
             investing.update_data()
             investing.render_window()
+            investing.update_window()
             investing.root.mainloop()
-            time.sleep(interval * 60)
-    elif mode == '2':
-        investing.update_data()
-        investing.render_window()
-        investing.update_window()
-        investing.root.mainloop()
-    else:
-        print('choose a valid mode...')
+        else:
+            print('choose a valid mode...')
